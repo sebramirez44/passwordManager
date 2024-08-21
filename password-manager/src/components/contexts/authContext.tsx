c7e850d7f35b8e5fe0d6606df3a66f08e626ec15
sebramirez44 <sebramirezcordero@gmail.com>
2024-08-21 05:08:35 -0600

    added auth provider

--- password-manager/src/components/contexts/authContext.tsx
+++ password-manager/src/components/contexts/authContext.tsx
@@ -1,8 +1,34 @@
-import {useContext, createContext, useState} from "react";
+//quiero que authContext contenga el username, userId
+// {
+//     username: string,
+//     userId: string
+// }
+//por ahora probar asi a ver si funciona bien el context, luego cambiar el type
+"use client"
+import { useRouter } from "next/navigation";
+import {createContext, useState, useEffect} from "react";
 
-const AuthContext = createContext<boolean>(false);
+export const AuthContext = createContext<boolean>(false);
 
-const AuthProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
+export const AuthProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    return <AuthContext.Provider value={isAuthenticated}>{children}</AuthContext.Provider>;
+    const router = useRouter();
+    useEffect(() => {
+        const checkAuthStatus = async () => {
+            setIsAuthenticated(false);
+            if (!isAuthenticated) {
+                router.push('/sign-in')
+            }
+        };
+        checkAuthStatus();
+    }, []);
+    //aqui se puede incluir un loading if (!isAuthenticated)
+    if (isAuthenticated) {
+        return (
+            <AuthContext.Provider value={isAuthenticated}>
+                {children}
+            </AuthContext.Provider>
+        );
+    }
+
 }
