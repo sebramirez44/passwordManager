77da728e274f5d9f427d87d9751f1f77888fba14
sebastian <sebastian@dhcp-128-189-217-233.ubcsecure.wireless.ubc.ca>
2024-09-02 20:30:53 -0700

    sidebar and router finished

--- password-manager/src/app/(mainPage)/layout.tsx
+++ password-manager/src/app/(mainPage)/layout.tsx
@@ -2,8 +2,7 @@ import PasswordManagerIconSecond from "@/icons/lock-second";
 import SearchComponent from "@/components/search";
 import Avatar from "@/components/avatar";
 import { AuthProvider } from "@/components/contexts/authContext";
-
-
+import Sidebar from "@/components/sidebar";
 
 export default function RootLayout({
   children,
@@ -13,11 +12,13 @@ export default function RootLayout({
   return (
         <AuthProvider>
             <div className="bg-[#EF233C] p-4 pt-3 flex flex-row justify-between px-10 items-center">
-            {/* Cargar el SVG del lock, luego el search, luego el boton del perfil y ya es todo */}
-            <PasswordManagerIconSecond size={5} />
-            <SearchComponent />
-            <Avatar/>
+                {/* Cargar el SVG del lock, luego el search, luego el boton del perfil y ya es todo */}
+                <PasswordManagerIconSecond size={5} />
+                <SearchComponent />
+                <Avatar/>
             </div>
+            <Sidebar/>
+
             {children}
         </AuthProvider>
   );
--- password-manager/src/components/sidebar.tsx
+++ password-manager/src/components/sidebar.tsx
@@ -1,5 +1,37 @@
+"use client"
+import { LockKeyhole } from "lucide-react";
+import { BookUser } from "lucide-react";
+import { Star } from "lucide-react";
+import { Trash2 } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+// maybe change the divs with onClick to be buttons in the future
+
 export default function Sidebar() {
-    return <>
-    
-    </>
+    const pathname = usePathname();
+    const router = useRouter();
+    return(
+        <div className="w-1/5 m-4 shadow rounded-b">
+            <div className="bg-[#EF233C] p-4 rounded-t">
+                <h1 className="text-white font-bold">Main Menu</h1>
+            </div>
+            <button type="button" onClick={() => {router.push("/")}} className={`w-full flex flex-row p-4 cursor-pointer hover:bg-slate-100 transition ${pathname==="/" ? "bg-slate-100" : ""}`}>
+                <LockKeyhole/>
+                <h2 className="ml-2">Passwords</h2>
+            </button>
+            <button type="button" onClick={() => {router.push("/websites")}} className={`w-full flex flex-row p-4 cursor-pointer hover:bg-slate-100 transition ${pathname==="/websites" ? "bg-slate-100" : ""}`}>
+                <BookUser/>
+                <h2 className="ml-2">Websites</h2>
+            </button>
+            <button type="button" onClick={() => {router.push("/favorites")}} className={`w-full flex flex-row p-4 cursor-pointer hover:bg-slate-100 transition ${pathname==="/favorites" ? "bg-slate-100" : ""}`}>
+                <Star/>
+                <h2 className="ml-2">Favorites</h2>
+            </button>
+            <button type="button" onClick={() => {router.push("/trash")}} className={`w-full flex flex-row p-4 cursor-pointer hover:bg-slate-100 transition ${pathname==="/trash" ? "bg-slate-100" : ""}`}>
+                <Trash2/>
+                <h2 className="ml-2">Trash</h2>
+            </button>
+        </div>
+    );
 };
